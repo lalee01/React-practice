151d4f3a3d00598968d9b2a6bc374d78cd3c64d8
lalee01 <sciro920119@gmail.com>
2021-06-14 23:01:19 +0200

    Boxes done

--- src/components/Second/Second.js
+++ src/components/Second/Second.js
@@ -2,22 +2,15 @@ import React, { useState} from 'react';
 
 const Second =({isRounded}) => {
     
-
     const [color , setColor] = useState(false)
     const onMouseOverHandler = () => setColor(!color)
     const onMouseOutHandler = () => setColor(!color)
-    const styleSet={
-        colorSetting :color ? {backgroundColor:"red"} : {} ,
-        roundSetting :isRounded ? {borderRadius: "50%"} : {}
 
-    }
-    console.log(styleSet.colorSetting)
-    console.log(styleSet.roundSetting)
+    const roundingStyle = isRounded ? {borderRadius: "50%"} : {};
+    const colorChangeStyle = color ? {backgroundColor: "red"} : {};
+    
     return (   
-        <div className="shape" style = {styleSet.colorSetting , styleSet.roundSetting}
-          /*{color ? {backgroundColor:'red'} : {} &
-                isRounded ? {borderRadius: '50%'} : {}*/
-        
+        <div className="shape" style={Object.assign(colorChangeStyle,roundingStyle)}
         id ="element-two" 
         onMouseOver={onMouseOverHandler} 
         onMouseOut={onMouseOutHandler}>
